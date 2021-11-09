b7a0ad3d0e88446381909d16ab750c3ae6bd82f4
Mamun-Developer <hridoy.developer@gmail.com>
2021-11-10 00:33:00 +0600

    session 2 complete

---
+++ session 2/basics.py
@@ -0,0 +1,12 @@
+def jay(a):
+    print(a)
+    print("Printed")
+
+
+def adder(a, b):
+    result = a + b
+    print(result)
+
+
+adder(10, 4)
+# jay("HEllo world")
---
+++ session 2/task1.py
@@ -0,0 +1,45 @@
+"""
+Task 1 :
+    Write a program that asks the user to enter a number and prints out a
+    message indicating whether the number is even or odd.
+"""
+
+
+def check_even_odd(number):
+    """
+    check if the number is even or odd
+    :param number:
+    :return: None
+    """
+    if number % 2 == 0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+
+    return 0
+
+
+number = input("Enter a number: ")
+# Type casting int(any_string_number)
+check_even_odd(int(number))
+
+"""
+Task 2 : Program to check if a number is prime or not
+"""
+
+"""
+Task 3 : Program to check if a string is palindrome or not
+"""
+
+"""
+Task 4 : Largest number among three numbers
+"""
+
+"""
+Task 5 : Program to check if a number is divisible by 5 or 11
+"""
+
+"""
+Task 6 : Show the arithmetic operations using any kind of codes [+,-,*,/,%]
+"""
+
